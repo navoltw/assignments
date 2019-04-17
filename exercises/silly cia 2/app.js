01872a7e179562239df7947d18f27f7e30c5b136
wayne navolt <navoltw@gmail.com>
2019-04-17 16:22:21 -0600

    daily planet

---
+++ exercises/silly cia 2/app.js
@@ -0,0 +1,18 @@
+// camelcase
+var myVariableIsNamedVar = 'this is my variable';
+
+//whitespace
+
+// var myVar = 'stuff'
+// if (myVar===things) {
+//     console.log(cool)
+// }else{
+//     console.log('dumb')
+// }
+
+// var prettySum = 4 * 3 / 2 + (5 - 1)
+
+// var person = {
+//     name: 'Bob'
+//     age: 32
+// }
---
+++ exercises/test/main.js
@@ -0,0 +1,38 @@
+var str = 'string';
+
+let strOne = 'string one'
+const strTwo = 'string two'
+
+var num = 2343
+
+var arr = ['random string', 4545, {hi: 1}, true, ['hi'], str, strOne, num]
+
+var foo = false
+
+var obj = {
+    hi: 1,
+    points: 0,
+}
+
+var mainHeader = ''
+
+var person = {
+    firstName: 'Wayne'
+    lastName: 'Wayne'
+    age: '8'
+
+}
+
+function myFunc(params) {
+    return k
+}
+
+function myNewFunc(numOne, numTwo) {
+    return numOne + numTwo
+}
+
+console.log(myNewFunc(3, 4))
+
+
+//parameters are placeholders used when declaring functions
+//arguments are values passed into a function
