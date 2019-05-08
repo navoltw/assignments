ac2af30a0c6147b593a21a8372ff559dd894f6fe
wayne navolt <navoltw@gmail.com>
2019-05-08 15:55:22 -0600

    colossal adventure

---
+++ projects/notes/axios/APP.JS/app.js
@@ -0,0 +1,26 @@
+// mkdir project-name
+// cd project-name
+// npm init -y
+// touch .gitignore
+//     Add node_modules/
+// npm i axios
+
+// const axios = require('axios')
+
+// axios.get(argumentOne)
+//     argumentOne is the url with the correct endpoint for the information you WebAuthentication
+
+// .then(argumentOne) will await the promised request before firing
+//     argumentOne should be a function called if the promise is fulfilled
+//     argumentTwo is optional and should be a function that handles the rejection reason
+
+// axios.post(argumentOne, argumentTwo)
+//     argumentOne is the url with the correct endpoint
+//     argumentTwo is the data you are sending, typically an Object
+
+// axios.delete(argumentOne, argumentTwo)
+//     argumentOne is the url with the correct endpoint
+
+// axios.put(argumentOne,argumentTwo)
+//     argumentOne is the url with the correct endpoint
+//     argumentTwo is an object containing listed changes
