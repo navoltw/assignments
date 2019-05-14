a667a756b8d29b38da5e34beef6cba7d217373be
wayne navolt <navoltw@gmail.com>
2019-05-14 11:50:45 -0600

    react

--- react/second app/my-react-app/src/App.js
+++ react/second app/my-react-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles.css'
 import './navbar/Navbar'
+import Navbar from './navbar/Navbar'
 
 const App = () => {
     return(
+      <>
+        <Navbar />
         <div className='main-container'>Hello World</div>
+      </>
     )
 }
 
